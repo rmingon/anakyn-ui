1e5038c1dcc895628c538e18e0a3b64ad689246b
rmingon <ronanmingon69550@gmail.com>
2023-02-07 07:00:48 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+
+/* eslint-disable import/prefer-default-export */
+
+export { default as AButton } from './Abutton.vue';
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.js'),
+      name: 'AnakynUI',
+      fileName: (format) => `anakyn-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
