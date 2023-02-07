ecbd2c9b242d411aedf077a87bf838dfe0be1efd
rmingon <ronanmingon69550@gmail.com>
2023-02-07 08:13:16 +0100

    add default color in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,25 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    color: {
+      'primary-red': '#D92525',
+      'secondary-red': '#8C1F28',
+      'tertiary-red': '#591C21',
+      'white': '#F2F2F2',
+      'primary-gray': '#044040',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      green: colors.green,
+      red: colors.red
+    },
     extend: {},
   },
   plugins: [],
 }
+
+
