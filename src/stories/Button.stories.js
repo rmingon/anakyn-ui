7f41fed8734fb949e886508c5a8ceddc798a926b
rmingon <ronanmingon69550@gmail.com>
2023-02-08 06:26:05 +0100

    add button storie

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,46 @@
+import {AButton} from '../components/index.js';
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Common/Button',
+  component: AButton,
+  argTypes: {
+    type: { control: 'select', options: ['primary', 'secondary', 'info', 'warning', 'error'] },
+    size: { control: 'select', options: ['s', 'm', 'l'] }
+  }
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  components: { AButton },
+  setup() {
+    return { args };
+  },
+  template: '<a-button v-bind="args" > {{ args.label }} </a-button>',
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Primary.args = {
+  type: 'primary',
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  type: 'secondary',
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'l',
+  type: 'info',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 's',
+  label: 'Button',
+};
