7cad316cc802e2c7f290f2b8f272674595ed34a2
rmingon <ronanmingon69550@gmail.com>
2023-02-08 06:26:22 +0100

    add tag storie

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@
 /* eslint-disable import/prefer-default-export */
 
 export { default as AButton } from './Abutton.vue';
+export { default as ATag } from './Atag.vue';
---
+++ src/stories/Tag.stories.js
@@ -0,0 +1,44 @@
+import { ATag } from '../components/index.js';
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Common/Tag',
+  component: ATag,
+  argTypes: {
+    type: { control: 'select', options: ['primary', 'secondary', 'info', 'warning', 'error'] },
+    size: { control: 'select', options: ['s', 'm', 'l'] }
+  }
+};
+
+const Template = (args) => ({
+  components: { ATag },
+  setup() {
+    return { args };
+  },
+  template: '<a-tag v-bind="args" > {{ args.label }} </a-tag>',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  type: 'primary',
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  type: 'secondary',
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'l',
+  type: 'info',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 's',
+  label: 'Button',
+};
