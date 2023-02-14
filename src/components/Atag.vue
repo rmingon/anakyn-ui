db17a86779b3c8e8c68770307e7e6eee7a73393d
rmingon <ronanmingon69550@gmail.com>
2023-02-14 06:10:40 +0100

    update tag

--- src/components/Atag.vue
+++ src/components/Atag.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="[size, borderless]" class="flex items-center">
+  <div :class="[size, type, borderless]" class="flex items-center cursor-default">
     <slot></slot>
     <div class="cursor-pointer" v-if="props.closable" @click="emit('close', event)">
       <svg xmlns="http://www.w3.org/2000/svg" :class="[cross_size]" viewBox="0 0 32 32"><path fill="currentColor" d="M24 9.4L22.6 8L16 14.6L9.4 8L8 9.4l6.6 6.6L8 22.6L9.4 24l6.6-6.6l6.6 6.6l1.4-1.4l-6.6-6.6L24 9.4z"/></svg>
@@ -16,19 +16,19 @@
   }
 
   const SIZE_CSS : Record<Size, string> = {
-    "s": 'text-xs px-2 py-1',
-    "m": 'px-4 py-2',
-    "l": 'text-xl px-5 py-3',
+    "s": 'text-xs px-1',
+    "m": 'px-2 py-1',
+    "l": 'text-xl px-3 py-2',
   } as const
 
   const TYPE_CSS : Record<Type, string> = {
-    "primary": 'bg-emerald-700 hover:bg-emerald-600 active:bg-emerald-500 text-white',
-    "secondary": 'text-zinc-500 hover:text-zinc-700 bg-gray-200 hover:bg-gray-100',
-    "tertiary": 'text-zinc-400 hover:text-zinc-500 hover:border-gray-300',
-    "success": 'bg-green-700 hover:bg-green-600 active:bg-green-500 text-white',
-    "info": 'bg-sky-700 hover:bg-sky-600 active:bg-sky-500 text-white',
-    "warning": 'bg-yellow-700 hover:bg-yellow-600 active:bg-yellow-500 text-white',
-    "error": 'bg-red-700 hover:bg-red-600 active:bg-red-500 text-white font-roboto',
+    "primary": 'bg-emerald-700 text-white',
+    "secondary": 'text-zinc-500 bg-gray-200',
+    "tertiary": 'text-zinc-400',
+    "success": 'bg-green-700 text-white',
+    "info": 'bg-sky-700 text-white',
+    "warning": 'bg-yellow-700 text-white',
+    "error": 'bg-red-700 text-white',
     "default": ''
   } as const
 
@@ -41,14 +41,14 @@
   const props = withDefaults(defineProps<Props>(), {
     type: 'primary',
     size: "m",
-    borderless: true,
+    borderless: false,
     closable: false
   })
 
   const size = SIZE_CSS[props.size]
   const cross_size = CLOSE_CSS_SIZE[props.size]
   const type = TYPE_CSS[props.type]
-  const borderless = props.borderless ? '' : 'border-2'
+  const borderless = props.borderless ? '' : 'border'
 
   const emit = defineEmits<{
     (e: 'close', event: Event): void
