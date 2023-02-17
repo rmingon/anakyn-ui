3e7142ecabd11d0c50227162f5c529ef6aadccbf
Ronan <ronanmingon69550@gmail.com>
2023-02-17 06:37:17 +0100

    add imports

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import '../assets/main.css'
 
 /* eslint-disable import/prefer-default-export */
 
-export { default as AButton } from './Abutton.vue';
-export { default as ATag } from './Atag.vue';
+export { default as AButton } from './Abutton.vue'
+export { default as ATag } from './Atag.vue'
+export { default as ACard } from './Acard.vue'
+export { default as AInput } from './Ainput.vue'
+export { default as AHeader } from './Aheader.vue'
