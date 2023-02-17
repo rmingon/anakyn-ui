89ae6118d8eb236a0439017b44bb5da7ae4241fe
Ronan <ronanmingon69550@gmail.com>
2023-02-17 06:36:12 +0100

    move style

--- src/components/Abutton.vue
+++ src/components/Abutton.vue
@@ -5,28 +5,13 @@
 </template>
 
 <script setup lang="ts">
+  import {SIZE_CSS, TYPE_CSS} from "@/components/MainStyle";
+
   interface Props {
     size: Size
     type: Type
     dashed?: boolean
   }
-
-  const SIZE_CSS : Record<Size, string> = {
-    "s": 'text-xs px-2 py-1',
-    "m": 'px-4 py-2',
-    "l": 'text-xl px-5 py-3',
-  } as const
-
-  const TYPE_CSS : Record<Props["type"], string> = {
-    "primary": 'border-black bg-emerald-700 hover:bg-emerald-600 active:bg-emerald-500 text-white',
-    "secondary": 'text-zinc-500 hover:text-zinc-700 bg-gray-200 hover:bg-gray-100',
-    "tertiary": 'text-zinc-400 hover:text-zinc-500 hover:border-gray-300',
-    "success": 'bg-green-700 hover:bg-green-600 active:bg-green-500 text-white',
-    "info": 'bg-sky-700 hover:bg-sky-600 active:bg-sky-500 text-white',
-    "warning": 'bg-yellow-700 hover:bg-yellow-600 active:bg-yellow-500 text-white',
-    "error": 'bg-red-700 hover:bg-red-600 active:bg-red-500 text-white font-roboto',
-    "default": ''
-  } as const
 
   const props = withDefaults(defineProps<Props>(), {
     size: "m",
@@ -37,4 +22,5 @@
   const size = SIZE_CSS[props.size]
   const type = TYPE_CSS[props.type]
   const border = props.dashed ? 'border-dashed border' : 'border-solid border'
+
 </script>
--- src/components/Atag.vue
+++ src/components/Atag.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="[size, type, borderless]" class="flex items-center cursor-default">
-    <slot></slot>
+    {{ props.label }}
     <div class="cursor-pointer" v-if="props.closable" @click="emit('close', event)">
       <svg xmlns="http://www.w3.org/2000/svg" :class="[cross_size]" viewBox="0 0 32 32"><path fill="currentColor" d="M24 9.4L22.6 8L16 14.6L9.4 8L8 9.4l6.6 6.6L8 22.6L9.4 24l6.6-6.6l6.6 6.6l1.4-1.4l-6.6-6.6L24 9.4z"/></svg>
     </div>
@@ -8,41 +8,22 @@
 </template>
 
 <script setup lang="ts">
+  import {CLOSE_CSS_SIZE, SIZE_CSS, TYPE_CSS} from "@/components/MainStyle";
+
   interface Props {
     size: Size
     closable: boolean
     borderless: boolean
     type: Type
+    label: string
   }
-
-  const SIZE_CSS : Record<Size, string> = {
-    "s": 'text-xs px-1',
-    "m": 'px-2 py-1',
-    "l": 'text-xl px-3 py-2',
-  } as const
-
-  const TYPE_CSS : Record<Type, string> = {
-    "primary": 'bg-emerald-700 text-white',
-    "secondary": 'text-zinc-500 bg-gray-200',
-    "tertiary": 'text-zinc-400',
-    "success": 'bg-green-700 text-white',
-    "info": 'bg-sky-700 text-white',
-    "warning": 'bg-yellow-700 text-white',
-    "error": 'bg-red-700 text-white',
-    "default": ''
-  } as const
-
-  const CLOSE_CSS_SIZE : Record<Size, string> = {
-    "s": 'h-4 w-4',
-    "m": 'h-5 w-5',
-    "l": 'h-6 w-6',
-  } as const
 
   const props = withDefaults(defineProps<Props>(), {
     type: 'primary',
     size: "m",
     borderless: false,
-    closable: false
+    closable: false,
+    label: ''
   })
 
   const size = SIZE_CSS[props.size]
