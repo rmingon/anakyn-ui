311c9aa65a69172a790dfc6a65fa67649c8fadc6
rmingon <ronanmingon69550@gmail.com>
2023-02-18 14:46:32 +0100

    change import

--- src/components/Abutton.vue
+++ src/components/Abutton.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-  import {SIZE_CSS, TYPE_CSS} from "@/components/MainStyle";
+  import {SIZE_CSS, TYPE_CSS} from "./MainStyle";
 
   interface Props {
     size: Size
