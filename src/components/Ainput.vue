39da668584bb915bb5b79f002b25962814333221
Ronan <ronanmingon69550@gmail.com>
2023-03-07 06:33:55 +0100

    add input props

--- src/components/Ainput.vue
+++ src/components/Ainput.vue
@@ -1,14 +1,25 @@
 <template>
-  <input type="text">
+  <input type="text" @input="emits('update:modelValue', $event.target.value)" >
 </template>
 
 <script setup lang="ts">
+  import {SIZE_CSS} from "@/components/MainStyle";
 
   interface Props {
     modelValue: string
     size: Size
-
   }
+
+  const props = withDefaults(defineProps<Props>(), {
+    modelValue: "",
+    size: "m"
+  })
+
+  const size = SIZE_CSS[props.size]
+
+  const emits = defineEmits<{
+    (e: "update:modelValue", input: EventTarget): void
+  }>()
 
 
 </script>
